3b258ba2c30711e6ad7429754a2e3cbf66358f40
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-08 08:10:26 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -17,9 +17,13 @@ class Config:
 
     BOT_OWNER = "1227193881"
 
-    AUTH_USERS_TEXT = "1227193881"
+  AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
-    AUTH_USERS = "1227193881"
+    AUTH_USERS = [BOT_OWNER, 374321319] + (
+        [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
+        if AUTH_USERS_TEXT
+        else []
+    )
 
     VIDEO_DESCRIPTION = (
         os.environ.get("VIDEO_DESCRIPTION", "").replace("<", "").replace(">", "")
